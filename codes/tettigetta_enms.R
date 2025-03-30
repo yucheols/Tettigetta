26449e83a3d77409d0fbee59eddb7dfb9eb49504
Jimancicada <156995539+Jimancicada@users.noreply.github.com>
2025-03-30 17:48:00 +0900

    Update tettigetta_enms.R

--- codes/tettigetta_enms.R
+++ codes/tettigetta_enms.R
@@ -3,9 +3,17 @@ rm(list = ls(all.names = T))
 gc()
 
 # Download packages
-install.packages(metaRange)
-install.packages(terra)
+install.packages("dismo")
+install.packages("raster")
+
+install.packages("metaRange")
+install.packages("terra")
 
 # load packages
+library(dismo)
+library(raster)
 library(metaRange)
 library(terra)
+
+# Road env data
+climate_data <- stack("")
