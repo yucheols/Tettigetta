95a517f190923b02c6243e7136db60d69b7405e4
Jimancicada <156995539+Jimancicada@users.noreply.github.com>
2025-03-30 17:52:56 +0900

    Update tettigetta_enms.R

--- codes/tettigetta_enms.R
+++ codes/tettigetta_enms.R
@@ -5,7 +5,6 @@ gc()
 # Download packages
 install.packages("dismo")
 install.packages("raster")
-
 install.packages("metaRange")
 install.packages("terra")
 
@@ -15,5 +14,25 @@ library(raster)
 library(metaRange)
 library(terra)
 
+#### 01. introduction 
+#### part 1 ::: setting up a simulation
+
+# load landscape data and scale it
+raster_file <- system.file('ex/elev.tif', package = 'terra')
+r <- rast(raster_file)
+r <- scale(r, center = F, scale = T)
+
+# verification
+print(r)
+plot(r)
+
+
 # Road env data
 climate_data <- stack("")
+
+# Prepare for occurrence data
+species_occurrence <- data.frame(
+  longitude = c(), latitude = c()
+  )
+
+species_sp <- SpatialPoints(species_occurrence, proj4string = CRS("+proj=longlat +datum=WGS84"))
